468349403c7b983b20f5295e8c50749116393dde
Dennis Periquet <dperique@gmail.com>
2019-07-21 01:03:04 +0000

    Call pod-template.yaml for readability

--- kube-stack/files/makeCont.sh
+++ kube-stack/files/makeCont.sh
@@ -24,10 +24,10 @@ if [[ $anImage == "default" ]]; then
   anImage="localhost:5000/kube-stack/kuul_zuul_periodics:v2.8b-ssh"
 fi
 if [[ "$anAction" == "create" ]]; then
-  cat 64ssh.yaml | sed -e "s/SOMENAME/$aName/g" -e "s,SOMEIMAGE,$anImage,g" | kubectl apply --context $CONTEXT -f -
+  cat pod-template.yaml | sed -e "s/SOMENAME/$aName/g" -e "s,SOMEIMAGE,$anImage,g" | kubectl apply --context $CONTEXT -f -
 
 else
-  cat 64ssh.yaml | sed -e "s/SOMENAME/$aName/g" -e "s,SOMEIMAGE,$anImage,g" | kubectl delete --context $CONTEXT -f -
+  cat pod-template.yaml | sed -e "s/SOMENAME/$aName/g" -e "s,SOMEIMAGE,$anImage,g" | kubectl delete --context $CONTEXT -f -
 fi
 
 echo ""
