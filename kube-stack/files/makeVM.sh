a8492bc376e3d23bd4cd3fb073973bc7723f7c96
Dennis Periquet <dperique@gmail.com>
2019-07-21 00:09:26 +0000

    Create VMs for Kubespray v2.4, v2.7, and using kubeadm

---
+++ kube-stack/files/makeVM.sh
@@ -0,0 +1,6 @@
+# Make 5 VMs for a CI Kubernetes cluster.
+#
+aFile="wrig-ubuntu-16.04-minimal.yaml"
+for i in {1..5}; do cat $aFile |sed "s/AVALUE/ci-kubeadm$i/"| kubectl apply -f - ; done
+for i in {1..5}; do cat $aFile |sed "s/AVALUE/ci-kube-v27-$i/"| kubectl apply -f - ; done
+for i in {1..5}; do cat $aFile |sed "s/AVALUE/ci-kube-v24-$i/"| kubectl apply -f - ; done
