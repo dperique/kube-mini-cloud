73c741778e1655edc8b5efd0f995a70ae9a50abe
Dennis Periquet <dennis.periquet@ibm.com>
2019-07-30 07:06:48 -0400

    add script to make several VMs for k8s clusters

--- kube-stack/files/makeVM.sh
+++ kube-stack/files/makeVM.sh
@@ -4,3 +4,4 @@ aFile="wrig-ubuntu-16.04-minimal.yaml"
 for i in {1..5}; do cat $aFile |sed "s/AVALUE/ci-kubeadm$i/"| kubectl apply -f - ; done
 for i in {1..5}; do cat $aFile |sed "s/AVALUE/ci-kube-v27-$i/"| kubectl apply -f - ; done
 for i in {1..5}; do cat $aFile |sed "s/AVALUE/ci-kube-v24-$i/"| kubectl apply -f - ; done
+for i in {1..5}; do cat $aFile |sed "s/AVALUE/ci-kube-vv$i/"| kubectl apply -f - ; done
